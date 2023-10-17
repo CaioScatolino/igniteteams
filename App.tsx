b8cfe1281324ade2063af4d46d604546d884806c
Caio Scatolino <caio.scatolino@outlook.com>
2023-10-17 08:56:23 -0300

    Estilizando tela de Loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { Groups } from '@screens/Groups';
-import {ActivityIndicator} from 'react-native'
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Loading } from '@components/Loading';
 
 import theme from './src/theme'
 
@@ -15,7 +15,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
 
 
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
 
     </ThemeProvider>
   );
